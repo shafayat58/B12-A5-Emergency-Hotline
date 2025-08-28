2b0f94343620a10e90156562a7a2aca4d8a8ff30
Shafayat Hossain <shafayathossaincse@gmail.com>
2025-08-28 18:55:08 +0600

    add call history function

--- index.js
+++ index.js
@@ -1,49 +1,132 @@
-// Coins and hearts
-let coins = 100;
-let hearts = 0;
 
-// Counters
-let coinCounter = document.getElementById("coinCount");
-let heartCounter = document.getElementById("heartCount");
 
-// Heart icons click
-let heartIcons = document.getElementsByClassName("card-heart");
-for (let i = 0; i < heartIcons.length; i++) {
-  heartIcons[i].onclick = function () {
-    hearts += 1;
-    heartCounter.innerText = hearts;
-  };
-}
+  document.addEventListener('DOMContentLoaded', function () {
+  
+  var callHistory = []; 
 
-// Call buttons click
-let callButtons = document.getElementsByClassName("call-button");
-for (let i = 0; i < callButtons.length; i++) {
-  callButtons[i].onclick = function () {
+  
+  var coinCounter   = document.getElementById('coinCount');
+  var heartCounter  = document.getElementById('heartCount');
+  var clearBtn      = document.getElementById('clearHistory');
+  var historyList   = document.getElementById('callHistory');
 
-    // Get card element
-    let card = this.parentNode.parentNode;
+  
+  var coins  = parseInt(coinCounter ? coinCounter.innerText : '0', 10) || 0;
+  var hearts = parseInt(heartCounter ? heartCounter.innerText : '0', 10) || 0;
 
-    // Get service name & number directly
-    let serviceName = card.querySelector("h1").innerText;
-    let serviceNumber = card.querySelector("h1 + p, h1 + h1").innerText;
+ 
+  var heartIcons = document.getElementsByClassName('card-heart');
+  for (var i = 0; i < heartIcons.length; i++) {
+    heartIcons[i].onclick = function () {
+      hearts = hearts + 1;
+      if (heartCounter) {
+        heartCounter.innerText = hearts;
+      }
+    };
+  }
 
-    // Check coins
-    if (coins < 20) {
-      alert("Not enough coins!");
-      return;
+  
+  var copyButtons = document.getElementsByClassName('copy-btn');
+  for (var j = 0; j < copyButtons.length; j++) {
+    copyButtons[j].onclick = function () {
+      var number = this.getAttribute('data-number') || '';
+      if (navigator.clipboard && navigator.clipboard.writeText) {
+        navigator.clipboard.writeText(number).then(function () {
+          alert('Copied: ' + number);
+        }, function () {
+          alert('Number: ' + number);
+        });
+      } else {
+        alert('Number: ' + number);
+      }
+    };
+  }
+
+  
+  var callButtons = document.getElementsByClassName('call-btn');
+  for (var k = 0; k < callButtons.length; k++) {
+    callButtons[k].onclick = function () {
+      var name = this.getAttribute('data-label') || 'Unknown Service';
+      var number = this.getAttribute('data-number') || 'N/A';
+
+      
+      if (coins < 20) {
+        alert('Not enough coins to make a call. You need 20 coins.');
+        return;
+      }
+
+     
+      coins = coins - 20;
+      if (coinCounter) {
+        coinCounter.innerText = coins;
+      }
+
+      
+      alert('Calling ' + name + ' at ' + number);
+
+      
+      addToHistory(name, number);
+    };
+  }
+
+  
+  function addToHistory(name, number) {
+    var item = {
+    name: name,
+  number: number,
+  when: new Date().toLocaleTimeString()
+    };
+  callHistory.push(item);
+  renderHistory();
+  }
+
+  
+  function renderHistory() {
+    if (!historyList) return;
+
+  
+  historyList.innerHTML = '';
+
+  
+  for (var i = 0; i < callHistory.length; i++) {
+      var data = callHistory[i];
+
+  var row = document.createElement('div');
+    row.className = 'flex justify-between items-start p-3  bg-[#fafafa] p-3 rounded-[20px] shadow';
+
+  var left = document.createElement('div');
+
+  var nameDiv = document.createElement('div');
+  nameDiv.className = 'font-semibold';
+  nameDiv.innerText =  data.name;
+
+  var numberDiv = document.createElement('div');
+  numberDiv.className = 'text-[#5C5C5C]';
+  numberDiv.innerText =  data.number;
+
+  left.appendChild(nameDiv);
+  left.appendChild(numberDiv);
+
+  var right = document.createElement('div');
+  right.className = 'text-xs opacity-75';
+  right.innerText = data.when;
+
+  row.appendChild(left);
+  row.appendChild(right);
+
+  historyList.appendChild(row);
     }
-
-    coins -= 20;
-    coinCounter.innerText = coins;
-
-    alert("Calling " + serviceName + " at " + serviceNumber);
-
-    addToHistory(serviceName, serviceNumber);
-  };
-}
+  }
 
 
+  if (clearBtn) {
+    clearBtn.onclick = function () {
+      callHistory.length = 0; 
+      renderHistory();        
+    };
+  }
 
-
-
+  
+  renderHistory();
+});
 
